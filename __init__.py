77c08af0d0c0ed61c4e7c82876af00f4d190ab23
Edmundo Gonzalez Hernandez <mundo2599@outlook.com>
2020-12-14 00:24:42 -0600

    Inicializacion de repositorio

---
+++ __init__.py
@@ -0,0 +1,21 @@
+# BACKEND
+# TODO: Crear excel nuevo
+# TODO: Copiar materiales y precio de previo excel al crear
+# TODO: Ingresar materiales y subs
+# TODO: Leer excel
+# TODO: Capturar informacion del dia
+# TODO: Guardar informacion del dia en excel
+# TODO: Crear tablas de nueva semana
+# TODO: Cambiar precios de materiales
+# TODO: Guardar datos de programa en db o archivo y excel (linea actual, ultima fecha, precios, etc)
+# TODO: Cargar archivo al iniciar el programa
+
+# NUEVAS IMPLEMENTACIONES
+# TODO: Manejar una base de datos local o en linea
+# TODO: Descargar y subir archivo excel
+# TODO: Capturar ventas
+
+# FRONTEND
+# TODO: Vista de materiales y precios
+# TODO: Vista de compras y ventas
+# TODO: Vista de informacion de archivo y otros
